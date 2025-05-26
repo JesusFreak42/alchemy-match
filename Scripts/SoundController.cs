5e5fc61d1fd3903545ca525944dd0bb0dfdc4f75
JesusFreak42 <23066999+JesusFreak42@users.noreply.github.com>
2025-05-26 17:36:07 -0500

    Add ability to mute/unmute audio

--- Scripts/SoundController.cs
+++ Scripts/SoundController.cs
@@ -1,23 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundController : MonoBehaviour
 {
 
     [SerializeField] private AudioSource musicSource;
-    [SerializeField] private AudioSource effectSource;
+    [SerializeField] private AudioSource sfxSource;
 
     [SerializeField] private AudioClip music;
     [SerializeField] private AudioClip blockMove;
     [SerializeField] private AudioClip btnClick;
+    
+    private bool muted = false; //whether the game sound is muted
+    [SerializeField] private Image muteImg; //the muted/unmuted UI image
+    [SerializeField] private Sprite soundSprite;
+    [SerializeField] private Sprite mutedSprite;
+
+    private void Start(){
+        SetMute(PlayerPrefs.GetInt("soundMuted",0) == 1); //set the muted state based on saved player pref, defaulting to unmuted if no saved pref
+    }
+
+
+    public void ToggleMute(){
+        SetMute(!muted);
+    }
+
+    private void SetMute(bool m){
+        muted = m;
+        PlayerPrefs.SetInt("soundMuted", muted ? 1 : 0); //save the muted state in player prefs
+
+        if (muted){
+            muteImg.sprite = mutedSprite;
+            musicSource.enabled = false;
+            sfxSource.enabled = false;
+        }
+        else{
+            muteImg.sprite = soundSprite;
+            musicSource.enabled = true;
+            sfxSource.enabled = true;
+        }
+    }
 
     public bool MusicIsPlaying(){
         return musicSource.isPlaying;
     }
 
     public bool EffectIsPlaying(){
-        return effectSource.isPlaying;
+        return sfxSource.isPlaying;
     }
 
     private void SetMusic(AudioClip c){
@@ -25,22 +56,28 @@ public class SoundController : MonoBehaviour
     }
 
     private void SetEffect(AudioClip c){
-        effectSource.clip = c;
+        sfxSource.clip = c;
     }
 
     public void PlayMusic(){
+        if (muted) return;
+        
         SetMusic(music);
         musicSource.Play();
     }
 
     public void PlayBlockMove(){
+        if (muted) return;
+        
         SetEffect(blockMove);
-        effectSource.Play();
+        sfxSource.Play();
     }
 
     public void PlayButtonClick(){
+        if (muted) return;
+        
         SetEffect(btnClick);
-        effectSource.Play();
+        sfxSource.Play();
     }
 
 }
